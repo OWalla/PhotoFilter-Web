43df8d3bf061eed36664a9c9c91009732a56cc30
Oded Dwek <oded.walla@gmail.com>
2016-05-08 23:12:12 +0300

    - Addded multer package, for supporting file uploads

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
+var multer = require('multer')
 
 // Set up the app
 var app = express();
