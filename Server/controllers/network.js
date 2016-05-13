7071b086d86a0f134a91ca014a2ab61facb9b039
RoiSudai <roisuda@gmail.com>
2016-05-13 14:57:28 +0300

    added train from zero to cnn

--- Server/controllers/network.js
+++ Server/controllers/network.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var Photo = require('../models/photo.js');
+var Cnn = require('../libs/cnn.js')
 var router = express.Router();
 
 /* GET home page. */
@@ -18,6 +20,20 @@ router.get('/getStartingNetworks', function(req, res) {
     }, ];
 
     res.json(networks);
+});
+router.get('/trainNetwork',function(req,res){
+  var photos = Photo.find({album: '573391407250282c2b33735e'}).exec(function(err, photosArray){
+    if (err)
+      console.log(err);
+    else {
+      var net = Cnn.trainFirstNetwork(photos);
+      var prediction = Cnn.predictImage(net, photosArray[10]);
+      console.log('prediction is: ' + prediction.w[0]);
+      res.send(net);
+    }
+
+  });
+
 });
 
 router.get('/getNetwork/:id', function(req, res) {
--- Server/libs/cnn.js
+++ Server/libs/cnn.js
@@ -1,13 +1,36 @@
 var convnetjs = require("convnetjs");
 
 function featuresToVolume(features) {
-  console.log(features);
     var x = new convnetjs.Vol([features.RedValue, features.GreenValue, features.BlueValue, features.Brightness, features.ColorBalance, features.SharpnessLevel, features.FacesInImageCount, features.AreFacesInImage]);
     console.log(x);
     return x;
 }
+exports.trainFirstNetwork = function(photosFeaturesList){
 
-exports.trainNetwork = function(network, photosFeatures) {
+    // define the network
+    var layer_defs = [];
+
+    layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:8});
+    layer_defs.push({type:'fc', num_neurons:5, activation:'relu'});
+    layer_defs.push({type:'softmax', num_classes:2});
+
+    // create the network
+    var net = new convnetjs.Net();
+    net.makeLayers(layer_defs);
+
+    // train the network
+    for (var i = 0; i < photosFeaturesList.length; i++) {
+        var featuresVolume = featuresToVolume(photosFeaturesList[i]);
+        net.train(featuresVolume, [photosFeaturesList[i].UserClassification]);
+    }
+
+    // network outputs all of its parameters into json object
+    var netJson = net.toJSON();
+    var netStr = JSON.stringify(netJson );
+    return netStr;
+}
+
+exports.trainNetwork = function(net, photosFeatures) {
     // Load the previous network data to the cnn model
     trainer = new convnetjs.SGDTrainer(net, {
         method: 'adadelta',
@@ -25,9 +48,13 @@ exports.trainNetwork = function(network, photosFeatures) {
     return net;
 }
 
-exports.predictImage = function(network, photoFeatures) {
+exports.predictImage = function(networkStr, photoFeatures) {
     var featuresVolume = featuresToVolume(photoFeatures);
 
-    var prediction = network.forward(featuresVolume);
+    var json = JSON.parse(networkStr); // creates json object out of a string
+    var net = new convnetjs.Net(); // create an empty network
+    net.fromJSON(json);
+
+    var prediction = net.forward(featuresVolume);
     return prediction;
 };
