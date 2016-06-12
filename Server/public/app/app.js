eadc50539327daccae7fe9cff1e6a0a6d487cbc4
Oded Dwek <oded.walla@gmail.com>
2016-06-13 01:38:16 +0300

    Added the authentication layer to angular and updated the routings.

--- Server/public/app/app.js
+++ Server/public/app/app.js
@@ -1,36 +1,56 @@
 (function() {
-  var app = angular.module("MainApp", ["ngRoute", "dndLists", "ngStorage"]);
+    var app = angular.module("MainApp", ["ngRoute", "dndLists", "ngStorage"]);
 
-  app.config(function($routeProvider) {
-    $routeProvider
-      .when("/main",
-      {
-        templateUrl: "views/main.html",
-        controller: "MainController"
-      })
-	  .when("/login", {
-        templateUrl: "views/login.html",
-        controller: "LoginController"
-      })
-	  .when("/register", {
-        templateUrl: "views/register.html",
-        controller: "LoginController"
-      })
-      .when("/album/:albumId", {
-        templateUrl: "views/album_view.html",
-        controller: "AlbumController"
-	  })
-      .when("/photo/:photoId", {
-        templateUrl: "views/view_photo.html",
-        controller: "PhotoController"
-      })
-      .when("/upload/", {
-        templateUrl: "views/upload_album.html",
-        controller: "UploadController"
-      })
-      .when("/about/", {
-        templateUrl: "views/about.html"
-      })
-      .otherwise({redirectTo:"/main"});
-  });
+    app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+        $routeProvider
+            .when("/main", {
+                templateUrl: "views/main.html",
+                controller: "MainController"
+            })
+            .when("/login", {
+                templateUrl: "views/login.html",
+                controller: "LoginController"
+            })
+            .when("/register", {
+                templateUrl: "views/register.html",
+                controller: "LoginController"
+            })
+            .when("/album/:albumId", {
+                templateUrl: "views/album_view.html",
+                controller: "AlbumController"
+            })
+            .when("/photo/:photoId", {
+                templateUrl: "views/view_photo.html",
+                controller: "PhotoController"
+            })
+            .when("/upload/", {
+                templateUrl: "views/upload_album.html",
+                controller: "UploadController"
+            })
+            .when("/about/", {
+                templateUrl: "views/about.html"
+            })
+            .otherwise({
+                redirectTo: "/main"
+            });
+
+        $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function($q, $location, $localStorage) {
+            return {
+                'request': function(config) {
+                    config.headers = config.headers || {};
+                    if ($localStorage.token) {
+                        config.headers.Authorization = 'Bearer ' + $localStorage.token;
+                    }
+                    return config;
+                },
+                'responseError': function(response) {
+                    if (response.status === 401 || response.status === 403) {
+                        $location.path('/signin');
+                    }
+                    return $q.reject(response);
+                }
+            }
+        }]);
+
+    }]);
 }());
