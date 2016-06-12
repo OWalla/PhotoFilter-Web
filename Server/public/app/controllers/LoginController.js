10da6c91575e1e882b23d92b0f50ba5fc4366f95
Oded Dwek <oded.walla@gmail.com>
2016-06-13 01:36:25 +0300

    Login and registration is now working.

--- Server/public/app/controllers/LoginController.js
+++ Server/public/app/controllers/LoginController.js
@@ -1,68 +1,66 @@
 (function() {
 
-  var app = angular.module("MainApp");
-  app.controller("LoginController", ["$rootScope", "$scope", "$window", '$localStorage', "$http", "Auth", LoginController]);
+    var app = angular.module("MainApp");
+    app.controller("LoginController", ["$rootScope", "$scope", "$window", '$localStorage', "$http", "Auth", LoginController]);
 
-  function LoginController($rootScope, $scope, $window, $localStorage, $http, Auth) {
+    function LoginController($rootScope, $scope, $window, $localStorage, $http, Auth) {
 
-    $http.get('/getStartingNetworks').
-    success(function(data) {
-      $scope.networks = data;
-    });
+        $scope.signin = function() {
+            var formData = {
+                username: $scope.username,
+                password: $scope.password
+            }
 
-    $scope.signin = function() {
-      var formData = {
-        email: $scope.email,
-        password: $scope.password
-      }
+            Auth.signin(formData, function(res) {
+                if (res.type == false) {
+                    alert(res.data)
+                } else {
+                    $localStorage.token = res.data.token;
+                    window.location = "/";
+                }
+            }, function() {
+                $rootScope.error = 'Failed to signin';
+            })
+        };
 
-      Auth.signin(formData, function(res) {
-        if (res.type == false) {
-          alert(res.data)
-        } else {
-          $localStorage.token = res.data.token;
-          window.location = "/";
-        }
-      }, function() {
-        $rootScope.error = 'Failed to signin';
-      })
-    };
+        $scope.signup = function() {
+            var formData = {
+                username: $scope.username,
+                password: $scope.password,
+                email: $scope.email,
+                first_name: $scope.first_name,
+                last_name: $scope.last_name
+            }
 
-    $scope.signup = function() {
-      var formData = {
-        email: $scope.email,
-        password: $scope.password
-      }
+            Auth.save(formData, function(res) {
+                if (res.type == false) {
+                    alert(res.data)
+                } else {
+                    $localStorage.token = res.data.token;
+                    window.location = "/"
+                }
+            }, function() {
+                $rootScope.error = 'Failed to signup';
+            })
+        };
 
-      Auth.save(formData, function(res) {
-        if (res.type == false) {
-          alert(res.data)
-        } else {
-          $localStorage.token = res.data.token;
-          window.location = "/"
-        }
-      }, function() {
-        $rootScope.error = 'Failed to signup';
-      })
-    };
+        $scope.me = function() {
+            Auth.me(function(res) {
+                $scope.myDetails = res;
+            }, function() {
+                $rootScope.error = 'Failed to fetch details';
+            })
+        };
 
-    $scope.me = function() {
-      Auth.me(function(res) {
-        $scope.myDetails = res;
-      }, function() {
-        $rootScope.error = 'Failed to fetch details';
-      })
-    };
+        $scope.logout = function() {
+            Auth.logout(function() {
+                window.location = "/"
+            }, function() {
+                alert("Failed to logout!");
+            });
+        };
 
-    $scope.logout = function() {
-      Auth.logout(function() {
-        window.location = "/"
-      }, function() {
-        alert("Failed to logout!");
-      });
-    };
+        $scope.token = $localStorage.token;
 
-    $scope.token = $localStorage.token;
-
-  }
+    }
 })();
