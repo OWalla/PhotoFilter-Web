8c814e9a28368f889e89594b0857c773d0deddc3
Oded Dwek <oded.walla@gmail.com>
2016-06-13 01:37:53 +0300

    You will now see only your albums.

--- Server/public/app/controllers/MainController.js
+++ Server/public/app/controllers/MainController.js
@@ -2,26 +2,26 @@
 
 (function() {
 
-  var app = angular.module("MainApp");
-  app.controller("MainController", ["$scope", "$http", MainController]);
+    var app = angular.module("MainApp");
+    app.controller("MainController", ["$scope", "$http", "$localStorage", "Auth", MainController]);
 
-  function MainController($scope, $http) {
+    function MainController($scope, $http, $localStorage, Auth) {
 
-    $http.get('/getNetwork/1').
-    success(function(data) {
-      $scope.network = data;
-
-      if (!$scope.network) {
-        $http.get('/getStartingNetworks').
+        $http.get('/getNetwork/1').
         success(function(data) {
-          $scope.networks = data;
-        });
-      }
-    });
+            $scope.network = data;
 
-    $http.get('/getUserAlbums/1').
-    success(function(data) {
-      $scope.albums = data;
-    });
-  };
+            if (!$scope.network) {
+                $http.get('/getStartingNetworks').
+                success(function(data) {
+                    $scope.networks = data;
+                });
+            }
+        });
+
+        $http.get('/getUserAlbums/' + Auth.getCurrentUser()._id).
+        success(function(data) {
+            $scope.albums = data;
+        });
+    };
 })();
