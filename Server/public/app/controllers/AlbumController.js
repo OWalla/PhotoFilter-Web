0069a0ad07c51f7fd06faf86cb0dbe28a5d93b82
Oded Dwek <oded.walla@gmail.com>
2016-06-12 19:22:12 +0300

    Fixed the album view, will now sort first based on the user classification and then by the network score.

--- Server/public/app/controllers/AlbumController.js
+++ Server/public/app/controllers/AlbumController.js
@@ -1,80 +1,89 @@
 // Code goes here
 
 (function() {
-  var app = angular.module("MainApp");
+    var app = angular.module("MainApp");
 
-  function AlbumController($scope, $http, $routeParams) {
+    function AlbumController($scope, $http, $routeParams) {
 
-    $scope.albumId = $routeParams.albumId;
+        $scope.albumId = $routeParams.albumId;
 
-    $scope.sendUpdates = function() {
-      $http.post('/sendUpdates', {
-        albumId: $scope.albumId,
-        classifications: $scope.models.lists
-      }).
-      success(function(data) {
-        alert("Send!");
-      });
+        $scope.sendUpdates = function() {
+            $http.post('/sendUpdates', {
+                albumId: $scope.albumId,
+                classifications: $scope.models.lists
+            }).
+            success(function(data) {
+                alert("Send!");
+            });
+        }
+
+        $scope.getThumbnail = function(fileName) {
+            var extensionIndex = fileName.lastIndexOf('.');
+            var fileExtension = fileName.substr(extensionIndex);
+            var fileName = fileName.substr(0, extensionIndex);
+            var thumbFile = fileName + "_thumb" + fileExtension;
+            return (thumbFile);
+        };
+
+        $scope.getClass = function(input) {
+            switch (input) {
+                case 'liked':
+                    return 'glyphicon glyphicon-thumbs-up';
+                    break;
+                case 'disliked':
+                    return 'glyphicon glyphicon-thumbs-down';
+                    break;
+            }
+            return "";
+        };
+
+        $scope.models = {
+            selected: null,
+            lists: {
+                "liked": [],
+                "disliked": []
+            }
+        };
+
+        $http.get('/getAlbum/' + $scope.albumId).
+        success(function(data) {
+            console.log(data);
+            $scope.albumName = data.albumName
+            for (index = 0; index < data.photos.length; ++index) {
+                var photo = data.photos[index];
+
+                if (photo.UserClassification) {
+                  switch (photo.UserClassification) {
+                      case 1:
+                          $scope.models.lists.liked.push(photo);
+                          break;
+
+                      case -1:
+                          $scope.models.lists.disliked.push(photo);
+                          break;
+                      default:
+                          alert('cyber');
+                          break;
+                  }
+                } else {
+
+                    switch (photo.networkScore) {
+                        case 1:
+                            $scope.models.lists.liked.push(photo);
+                            break;
+
+                        case -1:
+                            $scope.models.lists.disliked.push(photo);
+                            break;
+                        default:
+                            alert('cyber');
+                            break;
+                    }
+                }
+            }
+        });
     }
 
-    $scope.getThumbnail = function(fileName) {
-      var extensionIndex = fileName.lastIndexOf('.');
-      var fileExtension = fileName.substr(extensionIndex);
-      var fileName = fileName.substr(0, extensionIndex);
-      var thumbFile = fileName + "_thumb" + fileExtension;
-      return (thumbFile);
-    };
-
-    $scope.getClass = function(input) {
-      switch (input) {
-        case 'liked':
-          return 'glyphicon glyphicon-thumbs-up';
-          break;
-        case 'disliked':
-          return 'glyphicon glyphicon-thumbs-down';
-          break;
-        case 'default':
-          return 'glyphicon glyphicon-hourglass';
-          break;
-      }
-      return "";
-    };
-
-    $scope.models = {
-      selected: null,
-      lists: {
-        "liked": [],
-        "disliked": [],
-        "default": []
-      }
-    };
-
-    $http.get('/getAlbum/' + $scope.albumId).
-    success(function(data) {
-      console.log(data);
-      $scope.albumName = data.albumName
-      for (index = 0; index < data.photos.length; ++index) {
-        var photo = data.photos[index];
-
-        switch (photo.UserClassification) {
-          case 1:
-            $scope.models.lists.liked.push(photo);
-            break;
-
-          case 2:
-            $scope.models.lists.disliked.push(photo);
-            break;
-          case 0:
-            $scope.models.lists.default.push(photo);
-            break;
-          default:
-            alert('cyber');
-            break;
-        }
-      }
-    });
-  }
-
-  app.controller("AlbumController", ["$scope", "$http", '$routeParams', AlbumController]);
+    app.controller("AlbumController", ["$scope", "$http", '$routeParams', AlbumController]);
 
 })();
