7b508fc825a7f2913541b6bfd066e24114553ace
Oded Dwek <oded.walla@gmail.com>
2016-06-13 01:34:44 +0300

    Added auth library

---
+++ Server/libs/auth.js
@@ -0,0 +1,50 @@
+(function() {
+    exports.ensureAuthorized = function ensureAuthorized(req, res, next) {
+        var bearerToken;
+        var bearerHeader = req.headers["authorization"];
+        if (typeof bearerHeader !== 'undefined') {
+            var bearer = bearerHeader.split(" ");
+            bearerToken = bearer[1];
+            req.token = bearerToken;
+            next();
+        } else {
+            res.send(403);
+        }
+    }
+
+
+
+    function urlBase64Decode(str) {
+        var output = str.replace('-', '+').replace('_', '/');
+        switch (output.length % 4) {
+            case 0:
+                break;
+            case 2:
+                output += '==';
+                break;
+            case 3:
+                output += '=';
+                break;
+            default:
+                throw 'Illegal base64url string!';
+        }
+        return Buffer.from(output, 'base64');;
+    }
+
+    function getUserFromToken() {
+        var token = $localStorage.token;
+        var user = {};
+        if (typeof token !== 'undefined') {
+            var encoded = token.split('.')[1];
+            user = JSON.parse(urlBase64Decode(encoded));
+        }
+        return user;
+    }
+
+    exports.getUser = function(str) {
+        var encoded = str.split('.')[1];
+        user = JSON.parse(urlBase64Decode(encoded));
+
+        return user._doc;
+    };
+})();
