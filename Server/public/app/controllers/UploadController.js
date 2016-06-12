1f3b1048c88310a355eb2b89edb817dfbaf30cdf
Oded Dwek <oded.walla@gmail.com>
2016-06-13 01:37:07 +0300

    Added support to uploading an album with the user id.

--- Server/public/app/controllers/UploadController.js
+++ Server/public/app/controllers/UploadController.js
@@ -1,12 +1,15 @@
 (function () {
     var app = angular.module("MainApp");
 
-    function UploadController($scope, $http, $routeParams) {
+    function UploadController($scope, $http, $routeParams, Auth) {
+
+      $scope.user_id = Auth.getCurrentUser()._id;
+
         $http.get('/getSVMs').success(function (data) {
             $scope.svmNames = data;
         });
         $scope.chosenSVM;
     }
 
-    app.controller("UploadController", ["$scope", "$http", '$routeParams', UploadController]);
+    app.controller("UploadController", ["$scope", "$http", '$routeParams', "Auth", UploadController]);
 })();
