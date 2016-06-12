3bbe806ae11246b9c05ae4021694150a3e09cba2
Oded Dwek <oded.walla@gmail.com>
2016-06-12 10:03:53 +0300

    Updated the login controller, the login page will now communicate with the server.

--- Server/public/app/controllers/LoginController.js
+++ Server/public/app/controllers/LoginController.js
@@ -1,11 +1,68 @@
 (function() {
-	
-	var app = angular.module("MainApp");
-	 
-	function LoginController($scope, $http, $routeParams) {
 
-	}
+  var app = angular.module("MainApp");
+  app.controller("LoginController", ["$rootScope", "$scope", "$window", '$localStorage', "$http", "Auth", LoginController]);
 
-	app.controller("LoginController", ["$scope", "$http", '$routeParams', LoginController]);
-	
+  function LoginController($rootScope, $scope, $window, $localStorage, $http, Auth) {
+
+    $http.get('/getStartingNetworks').
+    success(function(data) {
+      $scope.networks = data;
+    });
+
+    $scope.signin = function() {
+      var formData = {
+        email: $scope.email,
+        password: $scope.password
+      }
+
+      Auth.signin(formData, function(res) {
+        if (res.type == false) {
+          alert(res.data)
+        } else {
+          $localStorage.token = res.data.token;
+          window.location = "/";
+        }
+      }, function() {
+        $rootScope.error = 'Failed to signin';
+      })
+    };
+
+    $scope.signup = function() {
+      var formData = {
+        email: $scope.email,
+        password: $scope.password
+      }
+
+      Auth.save(formData, function(res) {
+        if (res.type == false) {
+          alert(res.data)
+        } else {
+          $localStorage.token = res.data.token;
+          window.location = "/"
+        }
+      }, function() {
+        $rootScope.error = 'Failed to signup';
+      })
+    };
+
+    $scope.me = function() {
+      Auth.me(function(res) {
+        $scope.myDetails = res;
+      }, function() {
+        $rootScope.error = 'Failed to fetch details';
+      })
+    };
+
+    $scope.logout = function() {
+      Auth.logout(function() {
+        window.location = "/"
+      }, function() {
+        alert("Failed to logout!");
+      });
+    };
+
+    $scope.token = $localStorage.token;
+
+  }
 })();
