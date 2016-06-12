3a5df1540b294c5e31d29ff80ba7a4b8644d257f
Simon Ldj <simon.ldj@gmail.com>
2016-06-12 14:44:06 +0300

    Update UserClassification enum

--- Server/models/userClassification.js
+++ Server/models/userClassification.js
@@ -1,5 +1,5 @@
 var Enum = require('enum');
 
-var UserClassification = new Enum({'Unknown': 0, 'Liked': 1, 'Disliked': 2});
+var UserClassification = new Enum({'Unknown': 0, 'Liked': 1, 'Disliked': -1});
 
 module.exports = UserClassification;
